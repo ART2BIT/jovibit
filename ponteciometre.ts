3cedc49bdff2fb1982a826d54e753a9b28e29b7d
DojimaDragon <64280573+werente@users.noreply.github.com>
2021-11-17 12:54:20 +0100

    más pruebas

--- ponteciometre.ts
+++ ponteciometre.ts
@@ -1,17 +1,1 @@
-/**
-* Custom blocks
-*/
-//% color=#EBEE4A icon="\uf140"
-namespace pontenciometre {
-        /**
-        * pontenciometre
-        */
-        //% blockId=pontenciometre block="proves"
-        //% weight=15
-    export function pontenciometre (): void {
-        
-        basic.showNumber(420)
-        basic.clearScreen()
-    }
 
-}
