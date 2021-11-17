6fb1837ea8a38cbcad4340d69b18ee93b1bfe034
DojimaDragon <64280573+werente@users.noreply.github.com>
2021-11-17 12:42:13 +0100

    Actualizar ponteciometre.ts

--- ponteciometre.ts
+++ ponteciometre.ts
@@ -3,6 +3,15 @@
 */
 //% color=#EBEE4A icon="\uf140"
 namespace pontenciometre {
-    basic.showString("Helloooooooo")
-    basic.clearScreen()
+        /**
+        * pontenciometre
+        */
+        //% blockId=pontenciometre block="proves"
+        //% weight=15
+    export function pontenciometre (): void {
+        
+        basic.showNumber(420)
+        basic.clearScreen()
+    }
+
 }
