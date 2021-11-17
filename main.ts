8b7043157bd513b4d8e35ddad23639a3f08821eb
DojimaDragon <64280573+werente@users.noreply.github.com>
2021-11-17 15:04:59 +0100

    todavía más pruebas

--- main.ts
+++ main.ts
@@ -1,26 +1,34 @@
 /**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+enum Unitat_Distancia {
+    //% block="mm" enumval=0
+
+    Unitat_Distancia_mm,
+
+    //% block="cm" enumval=1
+    Unitat_Distancia_cm,
+
+    //% block="polzada" enumval=2
+    Unitat_Distancia_inch,
+}
+
+/**
 * Custom blocks
 */
 //% color=#ffdc00 icon="\uf140"
 namespace JoviBit {
-    enum Unitat_Distancia {
-        //% block="mm" enumval=0
-
-        Unitat_Distancia_mm,
-
-        //% block="cm" enumval=1
-        Unitat_Distancia_cm,
-
-        //% block="polzada" enumval=2
-        Unitat_Distancia_inch,
-    }
+    
+    /**
+    * Obté la distància ultrasònica en la mesura seleccionada
+    */
+    //% blockId=sonarbit block="Distància ultrasònica en %Unitat_distancia |al|pin %pin"
+    //% weight=10
+    //% block
     export function sonarbit_distancia(unitat_distancia: Unitat_Distancia, pin: DigitalPin): number {
-        /**
-        * Obté la distància ultrasònica
-        */
-        //% blockId=sonarbit block="Distància ultrasònica en %unitat_distancia |al|pin %pin"
-        //% weight=10
-
+    
         // send pulse
         pins.setPull(pin, PinPullMode.PullNone)
         pins.digitalWritePin(pin, 0)
@@ -45,7 +53,7 @@ namespace JoviBit {
                 return Math.floor(distancia)  //cm
                 break
             case 2:
-                return Math.floor(distancia / 254)   //inch
+                return Math.floor(distancia / 254)  //inch
                 break
             default:
                 return Math.floor(distancia)
@@ -53,16 +61,15 @@ namespace JoviBit {
 
     }
     
-    
+    /**
+    * Potenciòmetre
+    */
+    //% blockId=pontenciometre block="proves"
+    //% weight=10
     export function pontenciometre(): void {
-        /**
-        * Potenciòmetre
-        */
-        //% blockId=pontenciometre block="proves"
-        //% weight=15
-
-        basic.showNumber(420)
+        basic.showString("Hello")
         basic.clearScreen()
     }
+    
 
 }
