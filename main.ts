ecb2e61458fbb42a7e3d566096bd9673cb069dab
Guacamaya2000 <64280573+werente@users.noreply.github.com>
2022-01-17 13:29:46 +0100

    implementando el servo

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ enum Unitat_Distancia {
 /**
 * Custom blocks
 */
-//% color=#e89606 icon="\f0e7"
+//% color=#e89606 icon="\uf0e7"
 namespace JoviBit {
 
     /**
@@ -75,19 +75,119 @@ namespace JoviBit {
             pins.digitalWritePin(pin, 0)
         }
     }
-    
-    // Mangado de Bitbot
     /**
-      * Se intenta no tocar el codigo interior.
-      * @param void
-      */
-    //% blockId="Servo" block="Inicializamos el Servo | %PwmOnlyPin"
-    //% weight=100
-    //% subcategory=Servo
-    /*export function servo(cosa: PwmOnlyPin ): void {
-        
-        servos.Servo.prototype.setAngle(90)
-        
+     * Servo
+     */
+    let PCA = 0x40;
+    let initI2C = false;
+    let i2cError = 0;
+    let SERVOS = 0x06;
 
-    }*/
+    let servoTarget: number[]=[];
+    let servoActual: number[]=[];
+    let servoCancel: boolean []=[];
+    
+
+    //helper
+    function initPCA(): void {
+        
+        let i2cData = pins.createBuffer(2);
+        initI2C = true;
+
+        i2cData[0] = 0;
+        i2cData[1] = 0x10;
+        pins.i2cWriteBuffer(PCA, i2cData, false);
+
+        i2cData[0] = 0xFE;
+        i2cData[1] = 101;
+        pins.i2cWriteBuffer(PCA, i2cData, false);
+
+        i2cData[0] =0;
+        i2cData[1] = 0x81;
+        pins.i2cWriteBuffer(PCA,i2cData, false);
+
+        for (let servo = 0; servo < 16; servo++){
+            i2cData[0] = SERVOS + servo*4+0;
+            i2cData[1] = 0x00;
+            pins.i2cWriteBuffer(PCA, i2cData, false);
+
+            servoTarget[servo]=0;
+            servoActual[servo]=0;
+            servoCancel[servo]=false;
+
+        }
+    }
+
+    /**
+     * inicializa todos(pines) los servos al angulo 0
+     */
+    //%blockId = "centraServos"
+    //%block="Centra todos los Servos"
+    //%subcategory=Servos
+    export function centraServos(): void{
+        for(let i=0; i<16; i++){
+            for (let i=0; i<16; i++){
+                setServo(i, 0);
+            }
+        }
+    }
+
+    /**
+     * establece la posición del servo
+     * @param servo Servo numero(0 to 15)
+     * @param angle degrees to turn servo(-90 to +90)
+     */
+    //%blockId="setServo" block="Establece el servo %servo| al angulo %angle"
+    //%weight=70
+    //%angle.min=-90 angle.max=+90
+    //%sybcategory=Servo
+    export function setServo(servo: number, angle:number):void{
+
+        setServoRaw(servo, angle);
+        servoTarget[servo] = angle;
+    }
+
+    function setServoRaw(servo: number, angle: number): void{
+
+        if(initI2C == false){
+            initPCA();
+        }
+        
+        let i2cData = pins.createBuffer(2);
+        let start = 0;
+        angle = Math.max (Math.min(90, angle), -90);
+        let stop = 369 + angle * 223 / 90;
+
+        i2cData[0] = SERVOS + servo*4 + 2;
+        i2cData[1] = (stop & 0xFF);
+        pins.i2cWriteBuffer(PCA, i2cData, false);
+
+        i2cData[0] = SERVOS + servo* 4 + 3;
+        i2cData[1] = (stop >> 8);
+        pins.i2cWriteBuffer(PCA, i2cData, false);
+        servoActual[servo] = angle;
+
+    }
+
+    /**
+     * Mueve el Servo a la Posición a la velocidad seleccionada
+     * @param servo Servo number(0 to 15)
+     * @param angle degrees to turn to (-90 to +90)
+     * @param speed degrees per second to move (1  to 1000) eg: 60
+     */
+    //%blockId="moveServo" block="mover Servo %servo| al angulo %angle| a la velocidad %speed| degrees/sec"
+    //%weight=70
+    //%angle.min=-90 angle.max=90
+    //%speed.min=1 speed.max=1000
+    //% subcategory = Servo
+    export function moverServo(servo: number, angle: number, speed:number): void{
+        let step = 1;
+        let delay = 10; 
+        if (servoTarget[servo] != servoActual[servo])
+        {
+            servoCancel[servo] = true;
+            while(servoCancel[servo])
+            basic.pause(1)
+        }
+    }
 }
