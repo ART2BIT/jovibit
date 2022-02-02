0026175bbc3a4dc647a18bb2e31c35f396509eab
werente <rocharodriguezandres@gmail.com>
2022-02-02 09:27:39 +0100

    funciones de modificacion de pin

--- main.ts
+++ main.ts
@@ -3,8 +3,6 @@
  * Read more at https://makecode.microbit.org/blocks/custom
  */
 
-import { pinsHelper } from './pxt_modules/pinsHelper';
-
 enum Unitat_Distancia {
   //% block="mm" enumval=0
   Unitat_Distancia_mm,
@@ -84,7 +82,7 @@ namespace JoviBit {
   //% subcategory=SonarBit
   export function sonarbit_distancia(
     unitat_distancia: Unitat_Distancia,
-    pin: pinsHelper.Pin
+    pin: Pin
   ): number {
     // send pulse
     let truePin: DigitalPin = pinsHelper.pinToDigitalPin(pin);
@@ -640,4 +638,44 @@ namespace JoviBit {
    * @param pin
    * @returns
    */
+}
+namespace pinsHelper {
+  export function pinToDigitalPin(pin: Pin): DigitalPin {
+    let truePin: DigitalPin;
+    if (pin === 0) {
+      truePin = DigitalPin.P0;
+    } else if (pin === 1) {
+      truePin = DigitalPin.P1;
+    } else if (pin === 2) {
+      truePin = DigitalPin.P2;
+    } else if (pin === 8) {
+      truePin = DigitalPin.P8;
+    } else if (pin === 13) {
+      truePin = DigitalPin.P13;
+    } else if (pin === 14) {
+      truePin = DigitalPin.P14;
+    } else if (pin === 15) {
+      truePin = DigitalPin.P15;
+    }
+    return truePin;
+  }
+  export function pinToAnalogPin(pin: Pin): AnalogPin {
+    let truePin: AnalogPin;
+    if (pin === 0) {
+      truePin = AnalogPin.P0;
+    } else if (pin === 1) {
+      truePin = AnalogPin.P1;
+    } else if (pin === 2) {
+      truePin = AnalogPin.P2;
+    } else if (pin === 8) {
+      truePin = AnalogPin.P8;
+    } else if (pin === 13) {
+      truePin = AnalogPin.P13;
+    } else if (pin === 14) {
+      truePin = AnalogPin.P14;
+    } else if (pin === 15) {
+      truePin = AnalogPin.P15;
+    }
+    return truePin;
+  }
 }
